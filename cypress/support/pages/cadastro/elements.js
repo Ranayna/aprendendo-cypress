5aa8379e4655fc2928457e82fa610988bc19f45a
Ranayna <ranayna-oliveira@outlook.com>
2023-09-01 14:06:28 -0300

    alteração do nome de um arquivo e finalização dos arquivos criados da pasta pages

--- cypress/support/pages/cadastro/elements.js
+++ cypress/support/pages/cadastro/elements.js
@@ -1,5 +1,7 @@
 //! cria uma const que vai armazenar todos os elementos do cadastro trazidos do arquivo "data-testes"
 //! para importa-lo para outro arquivo é necessario colocar o "export" na frente da "const"
+//! Sera base para que esses dados possam ser importados para novos testes. Desse modo, não temos que reescrever tudo.
+//! Não sendo mais necessario recriar tudo na mão
 export const ELEMENTS = {
     registerNow: '[data-test="register"]',
     email: '[data-test="email"]',
--- cypress/support/pages/cadastro/pagina-cadastro.js
+++ cypress/support/pages/cadastro/pagina-cadastro.js
@@ -1,10 +1,31 @@
 //todo: centraliza a interação dos cadastro
 //! necessario importar as infos criadas no arquivo "elements", esse importe é feito através de uma const
 
+//! Atribuido a essa constante uma requisição para o arquivo elements.js
 const el = require('./elements').ELEMENTS;
 
+//!Cria-se uma classe para a página de cadastro.
+class Cadastro {
+    //!adiciona a primeira interação (que é um metodo) com a página de cadastro
+    acessarPaginaDeCadastro(){
+        //! acessa a página do alurapic e seleciona o link para registrar
+        cy.visit('http://localhost:4200/#/home');
+        cy.get('[data-test="register"]').click();
+    }
 
-
+    //!Cria-se um novo metodo que vai preencher os campos do formulario 
+    preencherFormulario(){
+        cy.get('input[data-test="email"]').type('lllll@lll.com');
+        cy.get('input[data-test="fullName"]').type('Lllll Lllll');
+        cy.get('input[data-test="registerUserName"]').type('lllllllll');
+        cy.get('input[data-test="registerPassword"]').type('LllllLllll');
+    }
+    //!Cria-se um novo metodo para selecionar o botão de registrar. 
+    //!Essa será a ultima interação com a página de cadastro
+    submeterCadastro(){
+        cy.get('[data-test="btnRegister"]').click();
+    }
+}
 
 
 
